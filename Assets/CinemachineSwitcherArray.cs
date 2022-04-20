7141954057fe4cf98702b22fbc6cb35de9d24384
Markus Lange <markus@hybrosia.com>
2022-04-21 00:19:46 +0200

    Negative and Positive Working

--- Assets/CinemachineSwitcherArray.cs
+++ Assets/CinemachineSwitcherArray.cs
@@ -5,6 +5,7 @@ public class CinemachineSwitcherArray : MonoBehaviour
 {
 
     [SerializeField] private InputAction action;
+    [SerializeField] private InputAction action2;
    
     [SerializeField] private CinemachineVirtualCamera[] vcam;
 
@@ -13,6 +14,7 @@ public class CinemachineSwitcherArray : MonoBehaviour
     private void Start()
     {
         action.performed += _ => SwitchPriority();
+        action2.performed += _ => NegPri();
     }
 
     private void SwitchPriority ()
@@ -21,7 +23,22 @@ public class CinemachineSwitcherArray : MonoBehaviour
         priority = Mathf.Clamp(priority, 0, vcam.Length-1);
         vcam[priority].Priority += priority;
     }
+    private void NegPri ()
+    {
+        vcam[priority].Priority -= priority;
+        priority--;
+        priority = Mathf.Clamp(priority, 0, vcam.Length-1);
+    }
 
-    private void OnEnable() => action.Enable();
-    private void OnDisable() => action.Disable();
+    private void OnEnable()
+    {
+        action.Enable();
+        action2.Enable();
+    }
+
+    private void OnDisable()
+    {
+        action.Disable();
+        action2.Disable();
+    }
 }
